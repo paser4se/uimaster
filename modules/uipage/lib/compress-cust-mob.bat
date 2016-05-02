f1c654da021fa9be64431cb0984562f1a3d763c9
Shaolin Wu <shao-lin.wu@hp.com>
2016-05-02 10:30:13 +0800

    compress customized js.

---
+++ modules/uipage/lib/compress-cust-mob.bat
@@ -0,0 +1,6 @@
+java -jar yuicompressor-2.4.8.jar cust-mob/jquery.js -o cust-mob/jquery-min.js
+java -jar yuicompressor-2.4.8.jar cust-mob/jquery-dataTable.js -o cust-mob/jquery-dataTable-min.js
+java -jar yuicompressor-2.4.8.jar cust-mob/uimaster.js -o cust-mob/uimaster-min.js
+java -jar yuicompressor-2.4.8.jar cust-mob/uimaster-widget.js -o cust-mob/uimaster-widget-min.js
+
+
