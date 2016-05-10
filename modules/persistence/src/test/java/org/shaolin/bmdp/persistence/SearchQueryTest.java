394e55146222ca9d4a6ece37cc3c3da11c7c2346
Shaolin Wu <shao-lin.wu@hp.com>
2016-05-10 22:21:25 +0800

    avoid nested transactions not supported error

--- modules/persistence/src/test/java/org/shaolin/bmdp/persistence/SearchQueryTest.java
+++ modules/persistence/src/test/java/org/shaolin/bmdp/persistence/SearchQueryTest.java
@@ -1,13 +1,23 @@
 package org.shaolin.bmdp.persistence;
 
+import org.hibernate.Session;
 import org.junit.Test;
+import org.shaolin.bmdp.runtime.AppContext;
+import org.shaolin.bmdp.runtime.internal.AppServiceManagerImpl;
 
 public class SearchQueryTest {
 
 	@Test
 	public void sqContentTest() {
 		
+		AppContext.register(new AppServiceManagerImpl("test", SearchQueryTest.class.getClassLoader()));
 		
+		HibernateUtil.getSession();
+		HibernateUtil.getSession();
+		HibernateUtil.getSession();
+		HibernateUtil.getSession();
+		HibernateUtil.getSession();
+		HibernateUtil.releaseSession(HibernateUtil.getSession(), true);
 		
 	}
 	
